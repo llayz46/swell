3c58ceef16da433472114e91005a699a15b31079
llayz46 <layzer@outlook.fr>
2025-11-16 10:57:14 +0100

    style: enhance dialog styling with new shadow and background colors

--- resources/js/components/swell/product/product-quick-view-dialog.tsx
+++ resources/js/components/swell/product/product-quick-view-dialog.tsx
@@ -16,39 +16,39 @@ export function ProductQuickViewDialog({ product, open, onClose }: { product: Pr
     if (!product) return null;
 
     return (
-        <Dialog open={open} onOpenChange={open => !open && onClose()}>
-            <DialogContent className="flex gap-8 !max-w-3xl">
+        <Dialog open={open} onOpenChange={(open) => !open && onClose()}>
+            <DialogContent className="shadow-dialog flex !max-w-3xl gap-5 rounded-2xl border-transparent p-5">
                 {product.featured_image ? (
-                    <img className="block h-72 object-cover aspect-square rounded-sm" src={getStorageUrl(product.featured_image.url)} alt={product.featured_image.alt_text} />
+                    <img
+                        className="block aspect-square h-72 rounded-md border border-slate-light-alpha object-cover"
+                        src={getStorageUrl(product.featured_image.url)}
+                        alt={product.featured_image.alt_text}
+                    />
                 ) : (
-                    <span className="block h-72 aspect-square rounded-sm bg-muted"></span>
+                    <span className="block aspect-square h-72 rounded-md border border-slate-light-alpha bg-slate-light"></span>
                 )}
 
                 <DialogHeader className="gap-4">
-                    <DialogTitle className="mr-4">{product.brand.name} {product.name}</DialogTitle>
+                    <DialogTitle className="mr-4">
+                        {product.brand.name} {product.name}
+                    </DialogTitle>
 
                     {product.discount_price != null ? (
                         <div className="flex items-baseline gap-2">
                             <span className="text-2xl font-bold text-primary">{product.discount_price.toFixed(2)} €</span>
-                            <span className="text-base mb-auto line-through text-muted-foreground">{product.price.toFixed(2)} €</span>
+                            <span className="mb-auto text-base text-muted-foreground line-through">{product.price.toFixed(2)} €</span>
                         </div>
                     ) : (
                         <span className="text-2xl font-bold text-primary">{product.price.toFixed(2)} €</span>
                     )}
 
-                    <DialogDescription>
-                        {product.short_description}
-                    </DialogDescription>
+                    <DialogDescription>{product.short_description}</DialogDescription>
 
-                    <Button
-                        className="mt-auto w-full rounded-sm"
-                        size="lg"
-                        onClick={() => addToCart(product)}
-                    >
+                    <Button className="mt-auto w-full rounded-md" size="lg" onClick={() => addToCart(product)}>
                         Ajouter au panier
                     </Button>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
-    )
+    );
 }
