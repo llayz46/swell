b0cafb2f8639400c9db0c90348e0f107cd4d7a9d
llayz46 <layzer@outlook.fr>
2025-12-06 15:25:17 +0100

    fix useActiveHook utilisation in nav-main

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -8,20 +8,28 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel>Liens</SidebarGroupLabel>
             <SidebarMenu>
-                {items.map((item) => {
-                    const isActive = useActiveNav(item.href);
-                    return (
-                        <SidebarMenuItem key={item.title}>
-                            <SidebarMenuButton asChild isActive={isActive} tooltip={{ children: item.title }}>
-                                <Link href={item.href} prefetch>
-                                    {item.icon && <item.icon />}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    );
-                })}
+                {items.map((item) => (
+                    <SidebarMenuItemWithActive 
+                        key={item.title} 
+                        item={item} 
+                    />
+                ))}
             </SidebarMenu>
         </SidebarGroup>
+    );
+}
+
+const SidebarMenuItemWithActive = ({ item }: { item: NavItem }) => {
+    const isActive = useActiveNav(item.href);
+
+    return (
+        <SidebarMenuItem>
+            <SidebarMenuButton asChild isActive={isActive} tooltip={{ children: item.title }}>
+                <Link href={item.href} prefetch>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                </Link>
+            </SidebarMenuButton>
+        </SidebarMenuItem>
     );
 }
